3e753139008d7bec8bfd1b8ef408cd2218126b61
pabloariel01 <pabloariel01@hotmail.com>
2019-02-09 22:10:37 -0300

    routes for api

--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,11 @@
 from flask import render_template,jsonify
 from app import app
 
+import lightpack_control as ctrl
 
 
-import lightpack, time
-lpack = lightpack.lightpack('127.0.0.1', 3636, list(range(50)),'{c2ef0ccd-dd8a-4e25-8ab3-8d17ce25b20c}' )
-lpack.connect()
 
-print("Lock: %s" % lpack.lock())
-print("turnOn: %s" % lpack.turnOn())
+
 
 
 @app.route('/')
@@ -22,13 +19,12 @@ def index():
 def info():
     return jsonify({
         'status': 'success',
-        'books': lpack.getAPIStatus()
+        'info': ctrl.getInfo()
     })
 
+@app.route('/getMode',methods=['GET'])
+def getMode():
+    return jsonify({
+        'mode': ctrl.getMode()
+    })
 
-
-def getInfo():
-    status={}
-    status.profiles=lpack.getProfiles()
-    status.actProfile=lpack.getProfile()
-    status.mode=lpack.getMode()
